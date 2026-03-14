a216148979bb79ea92401f6505e6d362504f24e8
elysia <3302843873@qq.com>
2026-03-14 10:27:07 +0800

    deleted whatever the misc/ bs is

--- misc/x86_64-seele.json/include/bits/locale-t.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RELIBC_BITS_LOCALE_T_H
-#define _RELIBC_BITS_LOCALE_T_H
-
-typedef void *locale_t;
-
-#endif  /* _RELIBC_BITS_LOCALE_T_H */
--- misc/x86_64-seele.json/include/bits/socklen-t.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RELIBC_BITS_SOCKLEN_T_H
-#define _RELIBC_BITS_SOCKLEN_T_H
-
-typedef uint32_t socklen_t;
-
-#endif  /* _RELIBC_BITS_SOCKLEN_T_H */
