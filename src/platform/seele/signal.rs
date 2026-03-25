07ee61026a6c754213b8961d506a0aefc0544962
elysia <3302843873@qq.com>
2026-03-25 20:44:50 +0800

    implemented kill and sigaction

--- src/platform/seele/signal.rs
+++ src/platform/seele/signal.rs
@@ -1,5 +1,11 @@
+use seele_sys::{
+    signal::{Signal, SignalAction, SignalHandlingType, Signals},
+    syscalls::signal::{register_signal_action, send_signal},
+    utils::process_result,
+};
+
 use crate::{
-    header::signal::sigval,
+    header::{netdb::protoent, signal::sigval},
     platform::{Pal, sys::e_raw},
 };
 use core::mem;
@@ -12,10 +18,42 @@ use crate::{
     error::{Errno, Result},
     header::{
         bits_time::timespec,
-        signal::{SI_QUEUE, sigaction, siginfo_t, sigset_t, stack_t},
+        signal::{SI_QUEUE, SIG_DFL, SIG_IGN, sigaction, siginfo_t, sigset_t, stack_t},
         sys_time::itimerval,
     },
 };
+
+impl From<&SignalAction> for sigaction {
+    fn from(action: &SignalAction) -> Self {
+        let sa_handler = match action.handling_type {
+            SignalHandlingType::Default => unsafe { core::mem::transmute(SIG_DFL) },
+            SignalHandlingType::Ignore => unsafe { core::mem::transmute(SIG_IGN) },
+            SignalHandlingType::Function(function) => Some(function),
+        };
+
+        Self {
+            sa_handler,
+            sa_flags: 0,
+            sa_restorer: None,
+            sa_mask: action.ignored_signals.bits(),
+        }
+    }
+}
+
+impl From<&sigaction> for SignalAction {
+    fn from(action: &sigaction) -> Self {
+        let handling_type = match action.sa_handler.map(|handler| handler as usize) {
+            Some(SIG_DFL) | None => SignalHandlingType::Default,
+            Some(SIG_IGN) => SignalHandlingType::Ignore,
+            Some(_) => SignalHandlingType::Function(action.sa_handler.unwrap()),
+        };
+
+        Self {
+            handling_type,
+            ignored_signals: Signals::from_bits_retain(action.sa_mask),
+        }
+    }
+}
 
 impl PalSignal for Sys {
     fn getitimer(which: c_int, out: &mut itimerval) -> Result<()> {
@@ -24,9 +62,9 @@ impl PalSignal for Sys {
     }
 
     fn kill(pid: pid_t, sig: c_int) -> Result<()> {
-        let _ = (pid, sig);
-        Sys::stub("KILL").map(|_| ())
+        e_raw(process_result(send_signal(pid as u64, Signal::from(sig)))).map(|_| ())
     }
+
     fn sigqueue(pid: pid_t, sig: c_int, val: sigval) -> Result<()> {
         let _ = (pid, sig, val);
         let _info = siginfo_t {
@@ -63,9 +101,26 @@ impl PalSignal for Sys {
         act: Option<&sigaction>,
         oact: Option<&mut sigaction>,
     ) -> Result<(), Errno> {
-        let _ = (sig, act, oact);
         if sig == 9 || sig == 19 {
             return Err(Errno(22));
+        }
+
+        let signal = Signal::from(sig);
+        let new_action = act.map(SignalAction::from);
+        let mut old_action = oact.as_ref().map(|_| SignalAction::default());
+
+        e_raw(process_result(register_signal_action(
+            signal,
+            new_action
+                .as_ref()
+                .map_or(core::ptr::null(), |action| action as *const SignalAction),
+            old_action
+                .as_mut()
+                .map_or(core::ptr::null_mut(), |action| action as *mut SignalAction),
+        )))?;
+
+        if let (Some(oact), Some(old_action)) = (oact, old_action.as_ref()) {
+            *oact = sigaction::from(old_action);
         }
 
         Ok(())
