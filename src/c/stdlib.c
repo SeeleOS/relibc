0d23d46222b4840c13c422b1c7c1e740f01002a9
elysia <3302843873@qq.com>
2026-04-08 10:28:04 +0800

    idk

--- src/c/stdlib.c
+++ src/c/stdlib.c
@@ -1,7 +1,13 @@
 double strtod(const char *nptr, char **endptr);
+typedef void *locale_t;
 
 long double strtold(const char *nptr, char **endptr) {
     return (long double)strtod(nptr, endptr);
+}
+
+long double strtold_l(const char *nptr, char **endptr, locale_t loc) {
+    (void)loc;
+    return strtold(nptr, endptr);
 }
 
 double relibc_ldtod(const long double* val) {
