a5c68d2502443649b231aa838a7a6a5844f559a9
elysia <3302843873@qq.com>
2026-04-10 21:57:58 +0800

    stddef: added WCHAR_T

--- include/stddef.h
+++ include/stddef.h
@@ -11,6 +11,16 @@ typedef __PTRDIFF_TYPE__ ptrdiff_t;
 
 typedef long unsigned int size_t;
 
+#ifndef _WCHAR_T
+#define _WCHAR_T
+#ifndef __cplusplus
+#ifndef __WCHAR_TYPE__
+#define __WCHAR_TYPE__ int
+#endif
+typedef __WCHAR_TYPE__ wchar_t;
+#endif
+#endif
+
 typedef struct { long long __ll; long double __ld; } max_align_t;
 
 #define offsetof(type, member) __builtin_offsetof(type, member)
