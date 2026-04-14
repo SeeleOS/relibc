91811df6ecb51a395259371470f3d5b3e604a06a
elysia <3302843873@qq.com>
2026-04-14 20:00:58 +0800

    stdbool: fix

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,19 +1,13 @@
 #ifndef _STDBOOL_H
 #define _STDBOOL_H
 
-#ifndef __cplusplus
-typedef _Bool bool;
+#if !defined(__cplusplus) && \
+    (!defined(__STDC_VERSION__) || __STDC_VERSION__ < 202000L)
+#define bool _Bool
 #define true 1
 #define false 0
-#else /* __cplusplus */
-typedef bool _Bool;
-#if __cplusplus < 201103L
-#define false false
-#define true true
-#endif /*__cplusplus < 201103L*/
-#endif /* __cplusplus */
+#endif
 
 #define __bool_true_false_are_defined 1
-
 
 #endif /* _STDBOOL_H */
