85a83a28500198496094e37e7a13be91439551f1
elysia <3302843873@qq.com>
2026-04-14 20:01:10 +0800

    added endian.h

---
+++ include/endian.h
@@ -0,0 +1,18 @@
+#ifndef _ENDIAN_H
+#define _ENDIAN_H
+
+#include <machine/endian.h>
+
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN BIG_ENDIAN
+#endif
+
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN LITTLE_ENDIAN
+#endif
+
+#ifndef __BYTE_ORDER
+#define __BYTE_ORDER BYTE_ORDER
+#endif
+
+#endif /* _ENDIAN_H */
